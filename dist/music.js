6ba8ce8e9391fc7be1acfc62e760467faebd6f79
kerwino <1746804529@qq.com>
2020-11-24 21:46:56 +0800

    update

--- dist/music.js
+++ dist/music.js
@@ -12,16 +12,22 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
+            name: '烦恼歌',
+            artist: '张学友',
+            url: 'http://m7.music.126.net/20201124211126/c6a9a82b47dc6c90ed18dde58bc44fa7/ymusic/4ed1/a3f0/6fdf/434b647d5965c6b668e3c089df4ee248.mp3',
+            cover: '/images/cover.png',
         },
         {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
+            name: '风的季节',
+            artist: 'Soler',
+            url: 'http://m7.music.126.net/20201124211505/3fd8931e90c7cd7fdaaab21e04b86c3e/ymusic/9f99/ddc9/462b/00ba269347ae7aee655557dc9c4a6720.mp3',
+            cover: '/images/cover.png',
+        },
+        {
+            name: '岁月神偷',
+            artist: '金玟岐',
+            url: 'http://m8.music.126.net/20201124212057/54908b74c784fb3875a913fad8b5956f/ymusic/10ad/ecdc/b6e0/1db87400ccb4d86ece611f5a17d72948.mp3',
+            cover: '/images/cover.png'
         }
     ]
 });
