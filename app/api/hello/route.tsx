ae0c4788e8fc50465d0640008dce30a554ca4b8e
Kerwino <kerwino.du@gmail.com>
2023-11-16 16:33:06 +0800

    x

--- app/api/hello/route.tsx
+++ app/api/hello/route.tsx
@@ -1,5 +1,8 @@
 export async function GET() {
     return new Response(JSON.stringify({ msg: "Hello" }, null, 4), {
         status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
     });
 }
