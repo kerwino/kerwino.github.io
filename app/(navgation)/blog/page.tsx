fd5db4a4939912f50cbe5ed19f5c34ffcec72804
Kerwino <kerwino.du@gmail.com>
2023-11-16 16:58:05 +0800

    feat

--- app/(navgation)/blog/page.tsx
+++ app/(navgation)/blog/page.tsx
@@ -1,9 +1,12 @@
+import { getPosts } from "@/app/api/getPosts";
 import SearchBar from "@/components/SearchBar";
 import { Suspense } from "react";
 export default function Post() {
+    const posts = getPosts();
     return (
         <article className={"prose w-full m-auto"}>
             <div>POSTS</div>
+            <div>{JSON.stringify(posts)}</div>
         </article>
     );
 }
