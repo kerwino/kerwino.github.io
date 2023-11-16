7efb42c88783c1ec6bd4b81c4709c13f04a77f33
Kerwino <kerwino.du@gmail.com>
2023-11-17 02:37:02 +0800

    feat:add code highlight

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,11 @@
 import type { MDXComponents } from "mdx/types";
+import { Code } from "bright";
+
+Code.lineNumbers = true;
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
+        pre: Code,
         ...components,
     };
 }
