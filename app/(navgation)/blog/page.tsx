fe6796ee886cefb9b1a030134a8ba9d2972bf22c
Kerwino <kerwino.du@gmail.com>
2023-11-16 15:26:02 +0800

    fix:headers

--- app/(navgation)/blog/page.tsx
+++ app/(navgation)/blog/page.tsx
@@ -3,10 +3,6 @@ import { Suspense } from "react";
 export default function Post() {
     return (
         <article className={"prose w-full m-auto"}>
-            <Suspense fallback={<h1>loading...</h1>}>
-                <SearchBar />
-            </Suspense>
-
             <div>POSTS</div>
         </article>
     );
