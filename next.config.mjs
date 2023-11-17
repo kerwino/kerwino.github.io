c11cf6a939ba93d07e4963893af766a16ad4c151
Kerwino <kerwino.du@gmail.com>
2023-11-17 08:27:05 +0800

    feat:auto set next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,32 @@ import remarkGfm from "remark-gfm";
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
     // Configure `pageExtensions` to include MDX files
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
     // Optionally, add any other Next.js config below
     transpilePackages: ["lucide-react"],
-    output: "export",
     experimental: {
         webpackBuildWorker: true,
     },
 };
+
+if (process.env.GITHUB_ACTIONS) {
+    let isPersonalPage = false;
+    const [user, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    if (repo === `${user}.github.io`) {
+        isPersonalPage = true;
+    }
+    /** @type {import('next').NextConfig} */
+    nextConfig = {
+        ...nextConfig,
+        output: "export",
+        images: {
+            unoptimized: true,
+        },
+        basePath: isPersonalPage ? "" : repo,
+    };
+}
 
 const withMDX = createMDX({
     // Add markdown plugins here, as desired
